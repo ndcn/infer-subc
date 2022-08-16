d4f925805e965c43c08fce78530353ba09111b79
Andy Henrie <arrrgo@gmail.com>
2022-08-16 10:00:17 -0700

    add env_create.sh

---
+++ env_create.sh
@@ -0,0 +1,12 @@
+
+# bioformats_jar was screwing things up. with OME
+conda create -n napari-env python=3.9 pip notebook 
+conda activate napari-env
+pip install 'napari[all]'
+pip install scipy scikit-learn matplotlib #jupyter
+pip install aicsimageio 
+pip install aicspylibczi
+pip install aicssegmentation #downgrades napari and scikitlearn
+
+pip install napari-aicsimageio  
+
