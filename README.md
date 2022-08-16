293eec7ab294f6ec231792db4a8de8d513fa42ac
Andy Henrie <arrrgo@gmail.com>
2022-08-15 22:39:02 -0700

    add notebooks and first base.py functions

--- README.md
+++ README.md
@@ -1,62 +1,49 @@
 
-# Python Project Template
-
-A low dependency and really simple to start project template for Python Projects.
-
-See also 
-- [Flask-Project-Template](https://github.com/rochacbruno/flask-project-template/) for a full feature Flask project including database, API, admin interface, etc.
-- [FastAPI-Project-Template](https://github.com/rochacbruno/fastapi-project-template/) The base to start an openapi project featuring: SQLModel, Typer, FastAPI, JWT Token Auth, Interactive Shell, Management Commands.
-
-### HOW TO USE THIS TEMPLATE
-
-> **DO NOT FORK** this is meant to be used from **[Use this template](https://github.com/rochacbruno/python-project-template/generate)** feature.
-
-1. Click on **[Use this template](https://github.com/rochacbruno/python-project-template/generate)**
-3. Give a name to your project  
-   (e.g. `my_awesome_project` recommendation is to use all lowercase and underscores separation for repo names.)
-3. Wait until the first run of CI finishes  
-   (Github Actions will process the template and commit to your new repo)
-4. If you want [codecov](https://about.codecov.io/sign-up/) Reports and Automatic Release to [PyPI](https://pypi.org)  
-  On the new repository `settings->secrets` add your `PIPY_API_TOKEN` and `CODECOV_TOKEN` (get the tokens on respective websites)
-4. Read the file [CONTRIBUTING.md](CONTRIBUTING.md)
-5. Then clone your new project and happy coding!
-
-> **NOTE**: **WAIT** until first CI run on github actions before cloning your new project.
-
-### What is included on this template?
-
-- 🖼️ Templates for starting multiple application types:
-  * **Basic low dependency** Python program (default) [use this template](https://github.com/rochacbruno/python-project-template/generate)
-  * **Flask** with database, admin interface, restapi and authentication [use this template](https://github.com/rochacbruno/flask-project-template/generate).
-  **or Run `make init` after cloning to generate a new project based on a template.**
-- 📦 A basic [setup.py](setup.py) file to provide installation, packaging and distribution for your project.  
-  Template uses setuptools because it's the de-facto standard for Python packages, you can run `make switch-to-poetry` later if you want.
-- 🤖 A [Makefile](Makefile) with the most useful commands to install, test, lint, format and release your project.
-- 📃 Documentation structure using [mkdocs](http://www.mkdocs.org)
-- 💬 Auto generation of change log using **gitchangelog** to keep a HISTORY.md file automatically based on your commit history on every release.
-- 🐋 A simple [Containerfile](Containerfile) to build a container image for your project.  
-  `Containerfile` is a more open standard for building container images than Dockerfile, you can use buildah or docker with this file.
-- 🧪 Testing structure using [pytest](https://docs.pytest.org/en/latest/)
-- ✅ Code linting using [flake8](https://flake8.pycqa.org/en/latest/)
-- 📊 Code coverage reports using [codecov](https://about.codecov.io/sign-up/)
-- 🛳️ Automatic release to [PyPI](https://pypi.org) using [twine](https://twine.readthedocs.io/en/latest/) and github actions.
-- 🎯 Entry points to execute your program using `python -m <infer_subc>` or `$ infer_subc` with basic CLI argument parsing.
-- 🔄 Continuous integration using [Github Actions](.github/workflows/) with jobs to lint, test and release your project on Linux, Mac and Windows environments.
-
-> Curious about architectural decisions on this template? read [ABOUT_THIS_TEMPLATE.md](ABOUT_THIS_TEMPLATE.md)  
-> If you want to contribute to this template please open an [issue](https://github.com/rochacbruno/python-project-template/issues) or fork and send a PULL REQUEST.
-
-[❤️ Sponsor this project](https://github.com/sponsors/rochacbruno/)
-
-<!--  DELETE THE LINES ABOVE THIS AND WRITE YOUR PROJECT README BELOW -->
-
 ---
 # infer_subc
 
 [![codecov](https://codecov.io/gh/ergonyc/infer-subc/branch/main/graph/badge.svg?token=infer-subc_token_here)](https://codecov.io/gh/ergonyc/infer-subc)
 [![CI](https://github.com/ergonyc/infer-subc/actions/workflows/main.yml/badge.svg)](https://github.com/ergonyc/infer-subc/actions/workflows/main.yml)
 
-Awesome infer_subc created by ergonyc
+Awesome infer_subc created by ergonyc to create a simple and extensible workflow of image analysis leveraging [scipy image](link), and [napari](link) for reproducable analysis with an intuitive interface. 
+
+This is a simple repo to collect code and documentations from the pilot project kicking off as part of the CZI Neurodegeneration Challenge Network [(NDCN)](https://chanzuckerberg.com/science/programs-resources/neurodegeneration-challenge/) Data Science Concierge program.  The PILOT study is a collaboration with Cohen lab at UNC [(website,](https://cohenlaboratory.web.unc.edu/) [github)](https://github.com/SCohenLab) to migrate a multispectral imaging dataset of iPSCs which identifies sub-cellular components to a scalable cloud-based pipeline.   
+
+--------------
+
+## Overview
+
+Notebooks  found [here]( link ) provide the template
+
+## Sub-Cellular object Inference PIPELINE OVERVIEW
+
+### GOAL:  Infer sub-cellular components in order to understand interactome 
+
+To measure shape, position, size, and interaction of eight organelles/cellular components (Nuclei (NU), Lysosomes (LS),Mitochondria (MT), Golgi (GL), Peroxisomes (PO), Endoplasmic Reticulum (ER), Lipid Droplet (LD), and SOMA) during differentiation of iPSCs, in order to understand the Interactome / Spatiotemporal coordination.
+
+### summary of _OBJECTIVES_
+- Infer subcellular objects:
+  -  #### #1. [infer NUCLEI ](#image-processing-objective-1-infer-nucleii) - NU, ch 0
+  -  #### #2. [Infer SOMA](#image-processing-objective-2-infer-soma) (extent of single brightest cell)- SO, composite icluding ch 1, ch 4,ch 5, and ch 7 "residuals"
+  -  #### #3. [Infer CYTOSOL](#image-processing-objective-3-infer-cytosol)- CY 
+  -  #### #4. [Infer LYSOSOMES](#image-processing-objective-4-infer-lysosomes)  - LS, ch 1
+  -  #### #5. [Infer MITOCHONDRIA](#image-processing-objective-5-infer-mitochondria) - MT, ch 2
+  -  #### #6. [Infer GOLGI complex](#image-processing-objective-6-infer-golgi-complex) - GL, ch 3
+  -  #### #7. [Infer PEROXISOMES](#image-processing-objective-7-infer-peroxisomes) - PO, ch  4
+  -  #### #8. [Infer ENDOPLASMIC RETICULUM ](#image-processing-objective-8-infer-endoplasmic-reticulum)- ER, ch 5
+  -  #### #9. [Infer LB](#image-processing-objective-9-infer-lipid-bodies-droplet), LD, ch 6 
+  -  
+
+## ADWB hints
+The medium term goal for this project is to execute it on ADDI's ADWB
+
+[uploading guide ](https://knowledgebase.aridhia.io/article/guidance-for-uploading-files/)
+[uploading files via the workspace article](https://knowledgebase.aridhia.io/article/uploading-files-via-the-workspace/).
+[Using BLOB storage](https://knowledgebase.aridhia.io/article/using-blob-storage/)
+
+### Uploading files to Blobs
+> The file upload to Blob storage follows the process described in [uploading files via the workspace article](https://knowledgebase.aridhia.io/article/uploading-files-via-the-workspace/). Note that due to the nature of Blob storage, folder hierarchies cannot exist without content. This means that you won't be able to create empty folders, and after refreshing the page the empty folders will be gone from your Blob storage. There is a workaround: you can create an empty folder, and without closing the window, add or upload a new file to the folder.
+
 
 ## Install it from PyPI
 
@@ -81,5 +68,11 @@ $ infer_subc
 ```
 
 ## Development
-
 Read the [CONTRIBUTING.md](CONTRIBUTING.md) file.
+
+The roadmap for this project includes extending to 
+### napari plugins 
+> insert link here
+
+
+### templates 
--- infer_subc/base.py
+++ infer_subc/base.py
@@ -3,15 +3,292 @@ infer_subc base module.
 
 This is the principal module of the infer_subc project.
 here you put your main classes and objects.
-
-Be creative! do whatever you want!
-
-If you want to replace this with a Flask application run:
-
-    $ make init
-
-and then choose `flask` as template.
 """
+import numpy as np
 
 # example constant variable
 NAME = "infer_subc"
+
+
+# these are the fuinctions that need to be set by each notebook...
+# notebook workflow will produce the in_params dictionary nescessary 
+# so all the images can be pushed through these functions (procedures)
+
+def infer_NUCLEI(struct_img, out_path, in_params):
+    pass
+
+def infer_SOMA(struct_img,  nuclei_labels, out_path, in_params):
+    pass
+
+def infer_CYTOSOL(struct_img, nuclei_labels, soma_labels, out_path, in_params):
+    pass
+
+def infer_LYSOSOMES(struct_img, out_path, cyto_labels, in_params):
+    pass
+
+def infer_MITOCHONDRIA(struct_img, out_path, cyto_labels, in_params):
+    pass
+
+def infer_GOLGI(struct_img, out_path, cyto_labels, in_params):
+    pass
+
+def infer_PEROXISOMES(struct_img, out_path, cyto_labels, in_params):
+    pass
+
+def infer_ENDOPLASMIC_RETICULUM(struct_img, out_path, cyto_labels, in_params):
+    pass
+
+def infer_LIPID_DROPLET(struct_img, out_path, cyto_labels, in_params):
+    pass
+
+
+#### Median FIltering for 2D
+# We need to define a wrapper for `median_filter` which steps through each Z-slice independently.  (Note: since we will use this 
+# pattern repeatedly we may want to make a generic wrapper for our filtering/de-noising). Lets call it `median_filter_slice_by_slice` 
+# and copy the way the `aicssegmentation` package handles smoothing.
+# TODO: typehints.... what is my "image" primitive?
+
+def median_filter_slice_by_slice(struct_img, size):
+    """
+    wrapper for applying 2D median filter slice by slice on a 3D image
+    """
+    from scipy.ndimage import median_filter
+    structure_img_denoise = np.zeros_like(struct_img)
+    for zz in range(struct_img.shape[0]):
+        structure_img_denoise[zz, :, :] = median_filter(struct_img[zz, :, :], size=size) 
+        
+    return structure_img_denoise
+
+
+def simple_intensity_normalization(struct_img, max_value=None):
+    """Normalize the intensity of input image so that the value range is from 0 to 1.
+
+    Parameters:
+    ------------
+    img: np.ndarray
+        a 3d image
+    max_value: float
+        
+    """
+    if max_value is not None:
+        struct_img[struct_img > max_value] = max_value
+
+    strech_min = struct_img.min()
+    strech_max = struct_img.max()
+
+    struct_img = (struct_img - strech_min + 1e-8) / (strech_max - strech_min + 1e-8)
+
+    return struct_img
+
+
+def read_input_image(image_name):
+    # from aicsimageio import AICSImage
+    # from napari_aicsimageio.core import _get_full_image_data, _get_meta
+    # img_in = AICSImage(czi_image_name)
+    # data_out = _get_full_image_data(img_in, in_memory=True)
+    # meta_out = _get_meta(data, img_in)
+    # meta_out['AICSImage'] = img_in
+
+    # prefer this wrapper because it returns numpy arrays
+    # or more simply with napari_aicsimagie io
+    from napari_aicsimageio.core import  reader_function
+    data_out, meta_out, layer_type = reader_function(image_name)[0]
+    return (data_out,meta_out)
+
+
+
+def log_transform(image):
+    """Renormalize image intensities to log space
+    
+    Returns a tuple of transformed image and a dictionary to be passed into
+    inverse_log_transform. The minimum and maximum from the dictionary
+    can be applied to an image by the inverse_log_transform to 
+    convert it back to its former intensity values.
+    """
+    orig_min, orig_max = scipy.ndimage.extrema(image)[:2]
+    #
+    # We add 1/2 bit noise to an 8 bit image to give the log a bottom
+    #
+    limage = image.copy()
+    noise_min = orig_min + (orig_max - orig_min) / 256.0 + np.finfo(image.dtype).eps
+    limage[limage < noise_min] = noise_min
+    d = {"noise_min": noise_min}
+    limage = np.log(limage)
+    log_min, log_max = scipy.ndimage.extrema(limage)[:2]
+    d["log_min"] = log_min
+    d["log_max"] = log_max
+    return stretch(limage), d
+
+
+def inverse_log_transform(image, d):
+    """Convert the values in image back to the scale prior to log_transform
+    
+    image - an image or value or values similarly scaled to image
+    d - object returned by log_transform
+    """
+    return np.exp(unstretch(image, d["log_min"], d["log_max"]))
+
+
+def stretch(image, mask=None):
+    """Normalize an image to make the minimum zero and maximum one
+    image - pixel data to be normalized
+    mask  - optional mask of relevant pixels. None = don't mask
+    returns the stretched image
+    """
+    image = np.array(image, float)
+    if np.product(image.shape) == 0:
+        return image
+    if mask is None:
+        minval = np.min(image)
+        maxval = np.max(image)
+        if minval == maxval:
+            if minval < 0:
+                return np.zeros_like(image)
+            elif minval > 1:
+                return np.ones_like(image)
+            return image
+        else:
+            return (image - minval) / (maxval - minval)
+    else:
+        significant_pixels = image[mask]
+        if significant_pixels.size == 0:
+            return image
+        minval = np.min(significant_pixels)
+        maxval = np.max(significant_pixels)
+        if minval == maxval:
+            transformed_image = minval
+        else:
+            transformed_image = (significant_pixels - minval) / (maxval - minval)
+        result = image.copy()
+        image[mask] = transformed_image
+        return image
+
+
+def unstretch(image, minval, maxval):
+    """Perform the inverse of stretch, given a stretched image
+    image - an image stretched by stretch or similarly scaled value or values
+    minval - minimum of previously stretched image
+    maxval - maximum of previously stretched image
+    """
+    return image * (maxval - minval) + minval
+
+
+def threshold_li_log( image_in ):
+    """
+    thin wrapper to log-scale and inverse log image for threshold finding using li minimum cross-entropy
+    """
+    from skimage.filters import threshold_li
+
+    image, d = log_transform(image_in.copy())
+    threshold =  threshold_li(image)
+    threshold = inverse_log_transform(threshold, d)
+    return threshold
+
+    image_out = log_transform(image_in.copy())
+    li_thresholded = structure_img_smooth >threshold_li_log( structure_img_smooth )
+
+def threshold_otsu_log( image_in ):
+    """
+    thin wrapper to log-scale and inverse log image for threshold finding using otsu
+    """
+    from skimage.filters import threshold_otsu
+
+    image, d = log_transform(image_in.copy())
+    threshold =  threshold_otsu(image)
+    threshold = inverse_log_transform(threshold, d)
+    return threshold
+
+def export_ome_tiff(data_in, meta_in, img_name, out_path, curr_chan=0) ->  str:
+    #  data_in: types.ArrayLike,
+    #  meta_in: dict,
+    # img_name: types.PathLike,
+    # out_path: types.PathLike,
+    # curr_chan: int
+    # assumes a single image
+    from aicsimageio.writers import OmeTiffWriter
+
+    out_name = out_path + img_name + ".ome.tiff"
+   
+    image_names = [img_name]
+    print(image_names)
+    #chan_names = meta_in['metadata']['aicsimage'].channel_names
+
+    physical_pixel_sizes = [ meta_in['metadata']['aicsimage'].physical_pixel_sizes ]
+
+    dimension_order = ["CZYX"]
+    channel_names= [ meta_in['metadata']['aicsimage'].channel_names[curr_chan] ]
+    if len(data_in.shape) == 3: #single channel zstack
+        data_in=data_in[np.newaxis,:,:,:]
+    
+    if data_in.dtype == 'bool':
+        data_in = data_in.astype(np.uint8)
+        data_in[ data_in > 0 ] = 255
+
+    out_ome = OmeTiffWriter.build_ome(
+                    [ data_in.shape],
+                    [data_in.dtype],
+                    channel_names=channel_names,  # type: ignore
+                    image_name=image_names,
+                    physical_pixel_sizes=physical_pixel_sizes,
+                    dimension_order=dimension_order,
+                )
+
+
+    OmeTiffWriter.save( data_in,
+                                        out_name,
+                                        dim_order=dimension_order,
+                                        channel_names = channel_names,
+                                        image_names = image_names,
+                                        physical_pixel_sizes = physical_pixel_sizes,
+                                        ome_xml=out_ome,
+                        )
+    return out_name
+
+
+### UTILS
+from collections import defaultdict
+def etree_to_dict(t):
+    d = {t.tag: {} if t.attrib else None}
+    children = list(t)
+    if children:
+        dd = defaultdict(list)
+        for dc in map(etree_to_dict, children):
+            for k, v in dc.items():
+                dd[k].append(v)
+        d = {t.tag: {k: v[0] if len(v) == 1 else v
+                     for k, v in dd.items()}}
+    if t.attrib:
+        d[t.tag].update(('@' + k, v)
+                        for k, v in t.attrib.items())
+    if t.text:
+        text = t.text.strip()
+        if children or t.attrib:
+            if text:
+                d[t.tag]['#text'] = text
+        else:
+            d[t.tag] = texts
+    return d
+
+
+
+def get_raw_meta_data(meta_dict):
+    from platform import system
+    curr_platform = system()
+
+    if curr_platform=='Linux':
+        raw_meta_data = meta_dict['metadata']['raw_image_metadata'].dict()
+        ome_types = meta_dict['metadata']['ome_types']
+    elif curr_platform=='Darwin':
+        raw_meta_data = meta_dict['metadata']['raw_image_metadata']
+        ome_types = []
+    else:
+        raw_meta_data = meta_dict['metadata']['raw_image_metadata']
+        ome_types = []
+        print(f"warning: platform = '{curr_platform}' is untested")
+    return (raw_meta_data,ome_types)
+
+
+
+
+
+
