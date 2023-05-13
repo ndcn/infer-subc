051f2a141cc1839711e7135d40ba60eabd40901d
andy henrie <ergonyc@gmail.com>
2023-05-13 00:26:23 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    use_scm = {"write_to": "organelle_segmenter_plugin/_version.py"}
+    use_scm = {"write_to": "infer_subc/_version.py"}
     setup(use_scm_version=use_scm)
 
 
