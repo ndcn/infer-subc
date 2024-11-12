c982563ed428564d699920e14d1efa412f7da474
shanrhoads <srhoads@ad.unc.edu>
2024-11-12 08:30:09 -0500

    install bug fix

--- env_create.sh
+++ env_create.sh
@@ -3,18 +3,15 @@ conda create -n infer-subc python=3.10
 conda activate infer-subc
 
 pip install napari[all]
-pip install CellProfiler
 
 pip install scipy scikit-learn matplotlib
-pip install aicsimageio 
+pip install aicsimageio
 pip install aicspylibczi
 pip install aicssegmentation 
 pip install napari-aicsimageio 
+pip install vispy
+pip install matlab
 pip install centrosome
 
 pip install infer-subc
 pip install organelle-segmenter-plugin
-
-pip install --upgrade tifffile
-pip install vispy
-pip install matlab
